d1542195f08122b39300f8521692087a83ef064f
arungalagali <53165333+arungalagali@users.noreply.github.com>
2019-07-24 13:39:18 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BankComponent } from './bank/bank.component';
+import { BankDetailComponent } from './bank/bankdetail.component';
+import { FilterPipe } from '.././assets/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankComponent,
+    FilterPipe,
+    BankDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
